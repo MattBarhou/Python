2671aa5a3a74cfd308b040f269532aac6ba32415
Matthew Barhou <barhoumatthew@gmail.com>
2024-03-09 13:17:22 -0500

    Quiz added and Turtle Graphic started

---
+++ Turtle Graphics/main.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color('green')
+
+
+    
+for i in range(100):
+    timmy.forward(100)
+    timmy.right(120)
+    timmy.forward(100)
+    timmy.right(90)
+
+        
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
