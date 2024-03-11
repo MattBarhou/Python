fa29729fa149e2f29eb4c8862bf42b89dd719ee1
Matthew Barhou <barhoumatthew@gmail.com>
2024-03-10 20:26:38 -0400

    Snake Game Complete

---
+++ Snake Game/main.py
@@ -0,0 +1,48 @@
+from turtle import Turtle, Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.tracer(0)
+screen.title("Welcome to the Snake Game")
+
+snake = Snake()
+food = Food()
+score = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+    
+game_on = True
+
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Check if snake collides with food
+    if snake.all_turtles[0].distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+        
+    # Check if snake collides with wall
+    if snake.all_turtles[0].xcor() > 290 or snake.all_turtles[0].xcor() < -290 or snake.all_turtles[0].ycor() > 290 or snake.all_turtles[0].ycor() < -290:
+        game_on = False
+        score.game_over()
+        
+    # Check if snake collides with itself
+    for segment in snake.all_turtles[1:]:
+        if snake.head.distance(segment) < 10:
+            game_on = False
+            score.game_over()
+
+screen.exitonclick()
---
+++ Snake Game/snake.py
@@ -0,0 +1,62 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    
+    def __init__(self):
+        self.all_turtles = []
+        self.create_snake()
+        self.head = self.all_turtles[0]
+    
+        
+    def create_snake(self):
+        # loop through the starting positions and add a turtle to each position
+        for position in STARTING_POSITIONS:
+            self.add_turtle(position)
+            
+    def add_turtle(self, position):
+        new_turtle = Turtle("square")
+        new_turtle.color("white")
+        new_turtle.penup()
+        new_turtle.goto(position)
+        self.all_turtles.append(new_turtle) 
+        
+    def extend(self):
+        # Get the position of the last segment in the snake
+        last_pos = self.all_turtles[-1].position()
+        # Add a new segment to the snake at this position
+        self.add_turtle(last_pos)
+
+        
+        
+    def move(self):
+        #loop through all segments and move the 2nd and 3rd segments to the position of the 1st segment
+        #then move the 1st segment forward
+        for s in range(len(self.all_turtles) - 1, 0, -1):
+            new_x = self.all_turtles[s - 1].xcor()
+            new_y = self.all_turtles[s - 1].ycor()
+            self.all_turtles[s].goto(new_x, new_y)
+        self.all_turtles[0].forward(20)
+            
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+            
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+            
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+            
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+            
+
+        
